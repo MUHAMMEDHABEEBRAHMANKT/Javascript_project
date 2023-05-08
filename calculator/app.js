2fc9873c8d7c145c1c5075ce89880ea839e0cbcc
MUHAMMED HABEEB RAHMAN KT <muhammadhabeebrahmam@gmail.com>
2023-05-08 20:24:44 +0530

    Add files via upload

---
+++ calculator/app.js
@@ -0,0 +1,25 @@
+let screen = document.getElementById("outbox");
+let clean = document.querySelector(".clean");
+let del = document.querySelector(".del");
+console.clear();
+function btclick(btvalue) {
+  screen.value += btvalue;
+}
+function eqbtn() {
+  if (screen.value === "") {
+    screen.value = "";
+  } else {
+    var text = screen.value;
+    var result = eval(text);
+    screen.value = result;
+  }
+}
+
+clean.onclick = function () {
+  screen.value = "";
+};
+
+del.onclick = function () {
+  screen.value = screen.value.slice(0, -1);
+  text = screen.value;
+};
